5a645dd9aa81ec7f43cf99fb75fe258388a66ac3
KaioAntonio <kaioantonio@bol.com.br>
2024-04-07 17:14:58 -0300

    feat: add consumer project

--- str-producer/src/main/java/com/kaio/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/kaio/strproducer/services/StringProducerService.java
@@ -13,7 +13,9 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message).addCallback(
+
+        kafkaTemplate.send("str-topic", message)
+                .addCallback(
                 success -> {
                     if(success != null) {
                         log.info("Send message with sucess {}", message);
