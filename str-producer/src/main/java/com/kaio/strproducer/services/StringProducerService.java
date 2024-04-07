94c9ffa7089f027660c2a904506ed4a8871cdfc3
KaioAntonio <kaioantonio@bol.com.br>
2024-04-07 14:58:02 -0300

    feat: add callback function to sendMessage function

--- str-producer/src/main/java/com/kaio/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/kaio/strproducer/services/StringProducerService.java
@@ -1,16 +1,26 @@
 package com.kaio.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class StringProducerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if(success != null) {
+                        log.info("Send message with sucess {}", message);
+                        log.info("Partition {}, Offset {}", success.getRecordMetadata().partition(),
+                                success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.info("Error send message"));
     }
 }
