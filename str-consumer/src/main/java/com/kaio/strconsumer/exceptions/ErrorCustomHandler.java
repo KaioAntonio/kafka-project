8e9dcc6c6e5234419fc88617d9b059299d22e496
KaioAntonio <kaioantonio@bol.com.br>
2024-04-10 11:58:46 -0300

    feat: add error handler

---
+++ str-consumer/src/main/java/com/kaio/strconsumer/exceptions/ErrorCustomHandler.java
@@ -0,0 +1,22 @@
+package com.kaio.strconsumer.exceptions;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class ErrorCustomHandler implements KafkaListenerErrorHandler {
+    @Override
+    public Object handleError(Message<?> message, ListenerExecutionFailedException e) {
+        log.info("Exception_Handler ::: Capturei um erro.");
+        return null;
+    }
+
+//    @Override
+//    public Object handleError(Message<?> message, ListenerExecutionFailedException exception, Consumer<?, ?> consumer) {
+//        return KafkaListenerErrorHandler.super.handleError(message, exception, consumer);
+//    }
+}
--- str-consumer/src/main/java/com/kaio/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/kaio/strconsumer/listeners/StrConsumerListener.java
@@ -1,6 +1,7 @@
 package com.kaio.strconsumer.listeners;
 
 import com.kaio.strconsumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Component;
 @Log4j2
 public class StrConsumerListener {
 
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
         log.info("Create ::: Receive message {}", message);
+        throw new IllegalArgumentException("Exception...");
     }
 
     @StrConsumerCustomListener(groupId = "group-1")
