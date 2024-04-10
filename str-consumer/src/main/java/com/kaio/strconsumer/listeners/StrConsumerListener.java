2f061e8e2e7f5bbf830b7063437f53b3d84e4c86
KaioAntonio <kaioantonio@bol.com.br>
2024-04-10 11:11:20 -0300

    feat: add record interceptor

--- str-consumer/src/main/java/com/kaio/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/kaio/strconsumer/listeners/StrConsumerListener.java
@@ -2,6 +2,7 @@ package com.kaio.strconsumer.listeners;
 
 import com.kaio.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,7 +19,7 @@ public class StrConsumerListener {
         log.info("Log ::: Receive message {}", message);
     }
 
-    @StrConsumerCustomListener(groupId = "group-2")
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
     public void history(String message) {
         log.info("HISTORY ::: Receive message {}", message);
     }
