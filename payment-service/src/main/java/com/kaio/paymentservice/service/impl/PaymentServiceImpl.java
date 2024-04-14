202b148a1606c7db738d184283d84bcc7802fc97
KaioAntonio <kaioantonio@bol.com.br>
2024-04-14 11:04:38 -0300

    feat: add producer config

--- payment-service/src/main/java/com/kaio/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/kaio/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,28 @@ package com.kaio.paymentservice.service.impl;
 
 import com.kaio.paymentservice.model.Payment;
 import com.kaio.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
-    @Override
-    public void sendPayment(Payment payment) {
-       log.info("PAYMENT_SERVICE_IMPL ::: Recebi o Pagamento {}", payment);
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @Override
+    @SneakyThrows
+    public void sendPayment(Payment payment) {
+       log.info("Recebi o Pagamento {}", payment);
+       Thread.sleep(2000);
+
+       log.info("Enviando pagamento ...");
+       kafkaTemplate.send("payment-topic", payment);
     }
 }
